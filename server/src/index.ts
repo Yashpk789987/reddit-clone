a09aa828833239738b5243d3023037c78c0345f7
Yashpk789987 <yashpk789987@gmail.com>
2020-09-26 21:22:58 +0530

    switeched from mikroorm to mikro-orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,29 @@
-import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import microConfig from "./mikro-orm.config";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
-import Redis from "ioredis";
-import session from "express-session";
-import connectRedis from "connect-redis";
-import { __prod__, COOKIE_NAME } from "./constants";
-import cors from "cors";
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
+import Redis from 'ioredis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { __prod__, COOKIE_NAME } from './constants';
+import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
+  const conn = await createConnection({
+    type: 'postgres',
+    database: 'reddit',
+    username: 'postgres',
+    password: 'postgres',
+    logging: true,
+    synchronize: true,
+    entities: [Post, User],
+  });
 
   const app = express();
 
@@ -24,7 +32,7 @@ const main = async () => {
 
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: 'http://localhost:3000',
       credentials: true,
     })
   );
@@ -39,11 +47,11 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
-        sameSite: "lax", // csrf
+        sameSite: 'lax', // csrf
         secure: __prod__, // cookie only works in http
       },
       saveUninitialized: false,
-      secret: "dkdk",
+      secret: 'dkdk',
       resave: false,
     })
   );
@@ -53,7 +61,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+    context: ({ req, res }) => ({ req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
@@ -62,7 +70,7 @@ const main = async () => {
   });
 
   app.listen(4000, () => {
-    console.log(" server started on localhost:4000");
+    console.log(' server started on localhost:4000');
   });
 };
 
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -7,16 +7,15 @@ import {
   ObjectType,
   Query,
 } from 'type-graphql';
-
 import { MyContext } from '../types';
-import { User_ } from '../entities/User';
+import { User } from '../entities/User';
 import argon2 from 'argon2';
-import { EntityManager } from '@mikro-orm/postgresql';
 import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
 import { validateRegister } from '../utils/validateRegister';
 import { sendEmail } from '../utils/nodemailer';
 import { v4 } from 'uuid';
+import { getConnection } from 'typeorm';
 
 @ObjectType()
 class FieldError {
@@ -31,50 +30,113 @@ class UserResponse {
   @Field(() => [FieldError], { nullable: true })
   errors?: FieldError[];
 
-  @Field(() => User_, { nullable: true })
-  user?: User_;
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => UserResponse)
+  async changePassword(
+    @Arg('token') token: string,
+    @Arg('newPassword') newPassword: string,
+    @Ctx() { redis, req }: MyContext
+  ): Promise<UserResponse> {
+    if (newPassword.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'newPassword',
+            message: 'length must be greater than 2',
+          },
+        ],
+      };
+    }
+
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
+    if (!userId) {
+      return {
+        errors: [
+          {
+            field: 'token',
+            message: 'token expired',
+          },
+        ],
+      };
+    }
+
+    const userIdNum = parseInt(userId);
+    const user = await User.findOne(userIdNum);
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'token',
+            message: 'user no longer exists',
+          },
+        ],
+      };
+    }
+
+    await User.update(
+      { id: userIdNum },
+      {
+        password: await argon2.hash(newPassword),
+      }
+    );
+
+    await redis.del(key);
+
+    // log in user after change password
+    req.session.userId = user.id;
+
+    return { user };
+  }
+
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg('email') email: string,
-    @Ctx() { em, redis }: MyContext
+    @Ctx() { redis }: MyContext
   ) {
-    const user = await em.findOne(User_, { email });
+    const user = await User.findOne({ where: { email } });
     if (!user) {
-      // email is not in db
+      // the email is not in the db
       return true;
     }
 
     const token = v4();
-    const key = FORGET_PASSWORD_PREFIX + token;
-    await redis.set(key, user.id, 'ex', 1000 * 60 * 60 * 24 * 3); // 3 days
 
-    sendEmail(
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      'ex',
+      1000 * 60 * 60 * 24 * 3
+    ); // 3 days
+
+    await sendEmail(
       email,
-      `<a href="http://localhost:3000/change-password/${token}" >Reset Password</a>`
+      `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
     );
 
     return true;
   }
 
-  @Query(() => User_, { nullable: true })
-  async me(@Ctx() { req, em }: MyContext) {
+  @Query(() => User, { nullable: true })
+  me(@Ctx() { req }: MyContext) {
     // you are not logged in
     if (!req.session.userId) {
       return null;
     }
 
-    const user = await em.findOne(User_, { id: req.session.userId });
-    return user;
+    return User.findOne(req.session.userId);
   }
 
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
     const errors = validateRegister(options);
     if (errors) {
@@ -84,18 +146,19 @@ export class UserResolver {
     const hashedPassword = await argon2.hash(options.password);
     let user;
     try {
-      const result = await (em as EntityManager)
-        .createQueryBuilder(User_)
-        .getKnexQuery()
-        .insert({
+      // User.create({}).save()
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values({
           username: options.username,
           email: options.email,
           password: hashedPassword,
-          created_at: new Date(),
-          updated_at: new Date(),
         })
-        .returning('*');
-      user = result[0];
+        .returning('*')
+        .execute();
+      user = result.raw[0];
     } catch (err) {
       //|| err.detail.includes("already exists")) {
       // duplicate username error
@@ -123,13 +186,12 @@ export class UserResolver {
   async login(
     @Arg('usernameOrEmail') usernameOrEmail: string,
     @Arg('password') password: string,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(
-      User_,
+    const user = await User.findOne(
       usernameOrEmail.includes('@')
-        ? { email: usernameOrEmail }
-        : { username: usernameOrEmail }
+        ? { where: { email: usernameOrEmail } }
+        : { where: { username: usernameOrEmail } }
     );
     if (!user) {
       return {
@@ -162,7 +224,7 @@ export class UserResolver {
 
   @Mutation(() => Boolean)
   logout(@Ctx() { req, res }: MyContext) {
-    return new Promise((resolve) => {
+    return new Promise((resolve) =>
       req.session.destroy((err) => {
         res.clearCookie(COOKIE_NAME);
         if (err) {
@@ -170,63 +232,9 @@ export class UserResolver {
           resolve(false);
           return;
         }
+
         resolve(true);
-      });
-    });
-  }
-
-  @Mutation(() => UserResponse)
-  async changePassword(
-    @Arg('token') token: string,
-    @Arg('newPassword') newPassword: string,
-    @Ctx() { redis, em, req }: MyContext
-  ): Promise<UserResponse> {
-    if (newPassword.length <= 2) {
-      return {
-        errors: [
-          {
-            field: 'newPassword',
-            message: 'length must be greater than 2',
-          },
-        ],
-      };
-    }
-    const key = FORGET_PASSWORD_PREFIX + token;
-    const userId = await redis.get(key);
-    if (!userId) {
-      return {
-        errors: [
-          {
-            field: 'token',
-            message: 'token expired',
-          },
-        ],
-      };
-    }
-
-    const user = await em.findOne(User_, { id: parseInt(userId) });
-
-    if (!user) {
-      return {
-        errors: [
-          {
-            field: 'token',
-            message: 'user no longer exists',
-          },
-        ],
-      };
-    }
-    user.password = await argon2.hash(newPassword);
-    await em.persistAndFlush(user);
-
-    await redis.del(key);
-
-    /// login user after chane password
-
-    req.session.userId = user.id;
-
-    return {
-      user,
-    };
+      })
+    );
   }
 }
